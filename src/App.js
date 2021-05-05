c6426ed28e9bb30c854da0ecc3035a4b6b4ece10
Philip Lau <philip@moesif.com>
2021-05-04 20:35:44 -0700

    remove uneecessary div

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Moesif Embedded Dashboard Implementation Example with React</h1>
-      <div>
-        <EmbeddedDash />
-      </div>
+      <EmbeddedDash />
     </div>
   );
 }
