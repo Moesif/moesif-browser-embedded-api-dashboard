872b08c08763a347cb2b8577a4d8ab22934c839a
Philip Lau <philip@moesif.com>
2021-07-08 14:54:04 -0700

    update embed test site

--- src/EmbeddedDisplay.js
+++ src/EmbeddedDisplay.js
@@ -33,6 +33,12 @@ export default function EmbeddedDisplay(props) {
   const [timeZone, setTimeZone] = useState("");
   const [hideChartAxisLabel, setChartAxisLabel] = useState(false);
   const [hideChartLegend, setChartLegend] = useState(false);
+  const [chartXAxisLabel, setChartXAxisLabel] = useState(undefined);
+  const [chartYAxisLabel, setChartYAxisLabel] = useState(undefined);
+  const [chartFontFamily, setChartFontFamily] = useState(undefined);
+  const [chartAxisFontSize, setChartAxisFontSize] = useState(undefined);
+  const [chartLabelFontSize, setChartLabelFontSize] = useState(undefined);
+  const [chartFontColor, setChartFontColor] = useState(undefined);
 
   const [finalUrl, setFinalUrl] = useState(dashEmbedInfo.url);
 
@@ -67,20 +73,6 @@ export default function EmbeddedDisplay(props) {
             onChange={(evt) => setHideHeader(!hideHeader)}
           />
           <br />
-          hide_chart_axis_label:{" "}
-          <input
-            type="checkbox"
-            checked={hideChartAxisLabel}
-            onChange={(evt) => setChartAxisLabel(!hideChartAxisLabel)}
-          />
-          <br />
-          hide_chart_legend:{" "}
-          <input
-            type="checkbox"
-            checked={hideChartLegend}
-            onChange={(evt) => setChartLegend(!hideChartLegend)}
-          />
-          <br />
           Add a line to time series chart with value:{" "}
           <input
             type="number"
@@ -94,9 +86,7 @@ export default function EmbeddedDisplay(props) {
             onChange={(evt) => setLineColor(evt.target.value)}
           />
           <br />
-          time zone (must be IANA timezone, most are supported, some rarely used
-          ones may not be supported by browsers). By default, browser timezone
-          is used, this allows you to override it.
+          time zone
           <select
             value={timeZone}
             onChange={(evt) => {
@@ -112,6 +102,69 @@ export default function EmbeddedDisplay(props) {
             <option value="UTC">UTC</option>
           </select>
           <br />
+          (must be IANA timezone, most are supported, some rarely used ones may
+          not be supported by browsers). By default, browser timezone is used,
+          this allows you to override it.
+          <br />
+          <br />
+          hide_chart_axis_label:{" "}
+          <input
+            type="checkbox"
+            checked={hideChartAxisLabel}
+            onChange={(evt) => setChartAxisLabel(!hideChartAxisLabel)}
+          />
+          <br />
+          hide_chart_legend:{" "}
+          <input
+            type="checkbox"
+            checked={hideChartLegend}
+            onChange={(evt) => setChartLegend(!hideChartLegend)}
+          />
+          <br />
+          chart_x_axis_label:{" "}
+          <input
+            type="text"
+            value={chartXAxisLabel}
+            onChange={(evt) => setChartXAxisLabel(evt.target.value)}
+          />
+          <br />
+          chart_y_axis_label:{" "}
+          <input
+            type="text"
+            value={chartYAxisLabel}
+            onChange={(evt) => setChartYAxisLabel(evt.target.value)}
+          />
+          <br />
+          FONTS
+          <br />
+          chart_font_family:{" "}
+          <input
+            type="text"
+            value={chartFontFamily}
+            onChange={(evt) => setChartFontFamily(evt.target.value)}
+          />
+          <br />
+          chart_font_color:{" "}
+          <input
+            type="text"
+            value={chartFontColor}
+            onChange={(evt) => setChartFontColor(evt.target.value)}
+          />
+          <br />
+          chart_axis_font_size:{" "}
+          <input
+            type="number"
+            value={chartAxisFontSize}
+            onChange={(evt) => setChartAxisFontSize(evt.target.value)}
+          />
+          <br />
+          chart_label_font_size:
+          <input
+            type="number"
+            value={chartLabelFontSize}
+            onChange={(evt) => setChartLabelFontSize(evt.target.value)}
+          />
+          <br />
           <button
             onClick={() => {
               const queryObject = {
@@ -120,6 +173,12 @@ export default function EmbeddedDisplay(props) {
                 hide_chart_axis_label: hideChartAxisLabel ? true : undefined,
                 hide_chart_legend: hideChartLegend ? true : undefined,
                 time_zone: timeZone ? timeZone : undefined,
+                chart_x_axis_label: chartXAxisLabel,
+                chart_y_axis_label: chartYAxisLabel,
+                chart_font_family: chartFontFamily,
+                chart_axis_font_size: chartAxisFontSize,
+                chart_label_font_size: chartLabelFontSize,
+                chart_font_color: chartFontColor,
                 drawing:
                   isNaN(lineValue) && !lineValue
                     ? undefined
